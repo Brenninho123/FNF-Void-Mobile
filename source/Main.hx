b0edcadd06989b5a9dd600798e680b8af39ffd46
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-06-14 18:37:12 -0300

    Atualizar o Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -80,7 +80,7 @@ class Main extends Sprite
 		initialState = Caching;
 		#end
 
-		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+		game = new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen);
 		addChild(game);
 
 		#if desktop
