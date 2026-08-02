aa03cbc5705645a01a7620f49ef952fbfa8a48e9
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-02 20:19:52 -0300

    Enhance Preloader with scaling and percent text
    Refactor Preloader class to include percentage display and responsive layout.

--- Preloader.hx
+++ Preloader.hx
@@ -1,57 +1,121 @@
-package ;
- 
+package;
+
 import flixel.system.FlxBasePreloader;
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
 import openfl.display.Sprite;
-import flash.display.Bitmap;
-import flash.display.BitmapData;
-import flash.display.BlendMode;
-import flash.display.Sprite;
-import flash.Lib;
-import flixel.FlxG;
- 
-@:bitmap("art/preloaderArt.png") class LogoImage extends BitmapData { }
- 
+import openfl.events.Event;
+import openfl.text.TextField;
+import openfl.text.TextFieldAutoSize;
+import openfl.text.TextFormat;
+import openfl.text.TextFormatAlign;
+import openfl.Lib;
+
+@:bitmap("art/preloaderArt.png") class LogoImage extends BitmapData {}
+
 class Preloader extends FlxBasePreloader
 {
-    public function new(MinDisplayTime:Float=3, ?AllowedURLs:Array<String>) 
-    {
-        super(MinDisplayTime, AllowedURLs);
-    }
-     
-    var logo:Sprite;
-     
-    override function create():Void 
-    {
-        this._width = Lib.current.stage.stageWidth;
-        this._height = Lib.current.stage.stageHeight;
-         
-        var ratio:Float = this._width / 2560; //This allows us to scale assets depending on the size of the screen.
-         
-        logo = new Sprite();
-        logo.addChild(new Bitmap(new LogoImage(0,0))); //Sets the graphic of the sprite to a Bitmap object, which uses our embedded BitmapData class.
-        logo.scaleX = logo.scaleY = ratio;
-        logo.x = ((this._width) / 2) - ((logo.width) / 2);
-        logo.y = (this._height / 2) - ((logo.height) / 2);
-        addChild(logo); //Adds the graphic to the NMEPreloader's buffer.
-         
-        super.create();
-    }
-     
-    override function update(Percent:Float):Void 
-    {
-        if(Percent < 69)
-        {
-            logo.scaleX += Percent / 1920;
-            logo.scaleY += Percent / 1920;
-            logo.x -= Percent * 0.6;
-            logo.y -= Percent / 2;
-        }else{
-            logo.scaleX = this._width / 1280;
-            logo.scaleY = this._width / 1280;
-            logo.x = ((this._width) / 2) - ((logo.width) / 2);
-            logo.y = (this._height / 2) - ((logo.height) / 2);
-        }
-        
-        super.update(Percent);
-    }
+	static inline var BASE_WIDTH:Float = 1280;
+	static inline var GROW_THRESHOLD:Float = 69;
+	static inline var GROW_AMOUNT:Float = 0.5;
+
+	var logo:Sprite;
+	var percentText:TextField;
+	var baseScale:Float = 1;
+
+	public function new(MinDisplayTime:Float = 3, ?AllowedURLs:Array<String>)
+	{
+		super(MinDisplayTime, AllowedURLs);
+	}
+
+	override function create():Void
+	{
+		_width = Lib.current.stage.stageWidth;
+		_height = Lib.current.stage.stageHeight;
+
+		Lib.current.stage.addEventListener(Event.RESIZE, onStageResize);
+
+		recalculateBaseScale();
+
+		logo = new Sprite();
+		logo.addChild(new Bitmap(new LogoImage(0, 0)));
+		logo.scaleX = logo.scaleY = baseScale;
+		addChild(logo);
+
+		percentText = new TextField();
+		percentText.selectable = false;
+		percentText.mouseEnabled = false;
+		percentText.autoSize = TextFieldAutoSize.CENTER;
+		percentText.defaultTextFormat = new TextFormat(null, 16, 0xFFFFFF, false, false, false, null, null, TextFormatAlign.CENTER);
+		percentText.text = "0%";
+		addChild(percentText);
+
+		layoutElements();
+
+		super.create();
+	}
+
+	function recalculateBaseScale():Void
+	{
+		baseScale = _width > 0 ? (_width / BASE_WIDTH) : 1;
+		if (baseScale <= 0)
+			baseScale = 1;
+	}
+
+	function layoutElements():Void
+	{
+		if (logo != null)
+		{
+			logo.x = (_width / 2) - (logo.width / 2);
+			logo.y = (_height / 2) - (logo.height / 2);
+		}
+
+		if (percentText != null)
+		{
+			var anchorY:Float = logo != null ? (logo.y + logo.height) : (_height / 2);
+			percentText.x = (_width / 2) - (percentText.width / 2);
+			percentText.y = anchorY + 12;
+		}
+	}
+
+	function onStageResize(e:Event):Void
+	{
+		_width = Lib.current.stage.stageWidth;
+		_height = Lib.current.stage.stageHeight;
+		recalculateBaseScale();
+		if (logo != null)
+			logo.scaleX = logo.scaleY = baseScale;
+		layoutElements();
+	}
+
+	override function update(Percent:Float):Void
+	{
+		var clamped:Float = Percent < 0 ? 0 : (Percent > 100 ? 100 : Percent);
+
+		if (logo != null)
+		{
+			if (clamped < GROW_THRESHOLD)
+			{
+				var eased:Float = easeOutQuad(clamped / GROW_THRESHOLD);
+				var scale:Float = baseScale * (1 + eased * GROW_AMOUNT);
+				logo.scaleX = logo.scaleY = scale;
+			}
+			else
+			{
+				logo.scaleX = logo.scaleY = baseScale;
+			}
+		}
+
+		layoutElements();
+
+		if (percentText != null)
+			percentText.text = Std.int(clamped) + "%";
+
+		super.update(clamped);
+	}
+
+	inline function easeOutQuad(t:Float):Float
+	{
+		return 1 - (1 - t) * (1 - t);
+	}
 }
